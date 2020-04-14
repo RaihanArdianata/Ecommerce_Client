65e8dd74738467990c924c2c6b793aa7dbfbccf4
RaihanArdianata <bxgxxxx@gmail.com>
2020-04-14 18:51:35 +0700

    2nd commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.css"
+import "animate.css/animate.css"
 
 Vue.config.productionTip = false
 
